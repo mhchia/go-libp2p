2e36882782744a1a6696e276c38d88f40e7933ae
mhchia <kevin.mh.chia@gmail.com>
2018-04-22 13:34:36 +0800

    Add forgotten files

---
+++ examples/minimal/addpeer.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+
+	inet "gx/ipfs/QmQm7WmgYCa4RSz76tKEYpRjApjnRw8ZTUVQC15b8JM4a2/go-libp2p-net"
+	pstore "gx/ipfs/QmeZVQzUrXqaszo24DAoHfGzcmCptN9JyngLkGAiEfk2x7/go-libp2p-peerstore"
+
+	pbmsg "github.com/libp2p/go-libp2p/examples/minimal/pb"
+
+	protobufCodec "github.com/multiformats/go-multicodec/protobuf"
+)
+
+// pattern: /protocol-name/request-or-response-message/version
+const addPeerRequest = "/addPeer/addpeerreq/0.0.1"
+const addPeerResponse = "/addPeer/addpeerresp/0.0.1"
+
+// AddPeerProtocol type
+type AddPeerProtocol struct {
+	node     *Node                            // local host
+	requests map[string]*pbmsg.AddPeerRequest // used to access request data from response handlers
+}
+
+func NewAddPeerProtocol(node *Node) *AddPeerProtocol {
+	p := &AddPeerProtocol{
+		node:     node,
+		requests: make(map[string]*pbmsg.AddPeerRequest),
+	}
+	node.SetStreamHandler(addPeerRequest, p.onRequest)
+	node.SetStreamHandler(addPeerResponse, p.onResponse)
+	return p
+}
+
+// remote peer requests handler
+func (p *AddPeerProtocol) onRequest(s inet.Stream) {
+
+	// get request data
+	data := &pbmsg.AddPeerRequest{}
+	decoder := protobufCodec.Multicodec(nil).Decoder(bufio.NewReader(s))
+	err := decoder.Decode(data)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("%s: Received ping request from %s. Message: %s", s.Conn().LocalPeer(), s.Conn().RemotePeer(), data.Message)
+
+	// generate response message
+	log.Printf("%s: Sending ping response to %s", s.Conn().LocalPeer(), s.Conn().RemotePeer())
+
+	resp := &pbmsg.AddPeerResponse{
+		Message: fmt.Sprintf("Ping response from %s", p.node.ID()),
+	}
+
+	// send the response
+	s, respErr := p.node.NewStream(context.Background(), s.Conn().RemotePeer(), addPeerResponse)
+	if respErr != nil {
+		log.Println(respErr)
+		return
+	}
+
+	ok := p.node.sendProtoMessage(resp, s)
+
+	if ok {
+		log.Printf("%s: Ping response to %s sent.", s.Conn().LocalPeer().String(), s.Conn().RemotePeer().String())
+	}
+}
+
+// remote ping response handler
+func (p *AddPeerProtocol) onResponse(s inet.Stream) {
+	data := &pbmsg.AddPeerResponse{}
+	decoder := protobufCodec.Multicodec(nil).Decoder(bufio.NewReader(s))
+	err := decoder.Decode(data)
+	if err != nil {
+		return
+	}
+	log.Printf("%s: Received ping response from %s", s.Conn().LocalPeer(), s.Conn().RemotePeer())
+	// locate request data and remove it if found
+	// _, ok := p.requests[data.MessageData.Id]
+	// if ok {
+	// 	// remove request from map as we have processed it here
+	// 	delete(p.requests, data.MessageData.Id)
+	// } else {
+	// 	log.Println("Failed to locate request data boject for response")
+	// 	return
+	// }
+}
+
+func (p *AddPeerProtocol) AddPeer(peerAddr string) bool {
+	peerid, targetAddr := parseAddr(peerAddr)
+	log.Printf("%s: Sending ping to: %s....", p.node.ID(), peerid)
+	p.node.Peerstore().AddAddr(peerid, targetAddr, pstore.PermanentAddrTTL)
+	// create message data
+	req := &pbmsg.AddPeerRequest{
+		Message: fmt.Sprintf("Ping from %s", p.node.ID()),
+	}
+
+	s, err := p.node.NewStream(context.Background(), peerid, addPeerRequest)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	if ok := p.node.sendProtoMessage(req, s); !ok {
+		return false
+	}
+
+	// store ref request so response handler has access to it
+	// p.requests[req.MessageData.Id] = req
+	// log.Printf("%s: Ping to: %s was sent. Message Id: %s, Message: %s", p.node.ID(), host.ID(), req.MessageData.Id, req.Message)
+	return true
+
+}
---
+++ examples/minimal/node.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"log"
+
+	inet "gx/ipfs/QmQm7WmgYCa4RSz76tKEYpRjApjnRw8ZTUVQC15b8JM4a2/go-libp2p-net"
+	"gx/ipfs/QmZ4Qi3GaRbjcx28Sme5eMH7RQjGkt8wHxt2a65oLaeFEV/gogo-protobuf/proto"
+	host "gx/ipfs/QmfCtHMCd9xFvehvHeVxtKVXJTMVTuHhyPRVHEXetn87vL/go-libp2p-host"
+
+	protobufCodec "github.com/multiformats/go-multicodec/protobuf"
+)
+
+// node client version
+const clientVersion = "go-p2p-node/0.0.1"
+
+// Node type - a p2p host implementing one or more p2p protocols
+type Node struct {
+	host.Host        // lib-p2p host
+	*AddPeerProtocol // ping protocol impl
+	// add other protocols here...
+}
+
+// Create a new node with its implemented protocols
+func NewNode(host host.Host) *Node {
+	node := &Node{Host: host}
+	node.AddPeerProtocol = NewAddPeerProtocol(node)
+	return node
+}
+
+// helper method - writes a protobuf go data object to a network stream
+// data: reference of protobuf go data object to send (not the object itself)
+// s: network stream to write the data to
+func (n *Node) sendProtoMessage(data proto.Message, s inet.Stream) bool {
+	writer := bufio.NewWriter(s)
+	enc := protobufCodec.Multicodec(nil).Encoder(writer)
+	err := enc.Encode(data)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	writer.Flush()
+	return true
+}
